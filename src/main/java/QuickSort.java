095e29e93d9a55e03e8c75a1ed129f114eda3149
bounis <bounis@olbati.com>
2019-11-18 00:12:52 +0100

    enh

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -9,6 +9,10 @@ public class QuickSort {
 
     static int[] quickSort(int[] arr, int idx) {
 
+        int pivot = arr[idx];
+        if (arr.length == 1) {
+            return arr;
+        }
         if (arr.length == 2) {
             if (arr[0] > arr[1]) {
                 int aux = arr[0];
@@ -20,10 +24,10 @@ public class QuickSort {
         int firstLength = 0;
         int secondLength = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (i != idx && arr[i] <= idx) {
+            if (i != idx && arr[i] <= arr[idx]) {
                 firstLength++;
             }
-            if (i != idx && arr[i] <= idx) {
+            if (i != idx && arr[i] > arr[idx]) {
                 secondLength++;
             }
         }
@@ -31,15 +35,34 @@ public class QuickSort {
         int[] firstTab = new int[firstLength];
         int[] secondTab = new int[secondLength];
 
-        if (firstLength > 1) {
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != idx && arr[i] <= arr[idx]) {
+                firstTab[j] = arr[i];
+            j++;
+            }
+            if (i != idx && arr[i] > arr[idx]) {
+                secondTab[k] = arr[i];
+                k++;
+            }
+        }
+
+
+        if (firstLength > 0) {
+
             int[] firstTabSorted = quickSort(firstTab, 0);
+            for (int i = 0; i < firstTabSorted.length; i++) {
+                arr[i] = firstTabSorted[i];
+            }
         }
-        int[] secondTabSorted = quickSort(secondTab, 0);
-        for (int i = 0; i < firstTabSorted.length; i++) {
-            arr[i] = firstTabSorted[i];
-        }
-        for (int i = 0; i < secondTabSorted.length; i++) {
-            arr[firstLength + 1 + i] = secondTabSorted[i];
+        arr[firstLength] = pivot;
+        if (secondLength > 0) {
+            int[] secondTabSorted = quickSort(secondTab, 0);
+            for (int i = 0; i < secondTabSorted.length; i++) {
+                arr[firstLength + 1 + i] = secondTabSorted[i];
+            }
+
         }
 
         return arr;
